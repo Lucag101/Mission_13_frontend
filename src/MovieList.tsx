930e45b71bae0401780070321947de1087efd8f5
Lucag101 <114105978+Lucag101@users.noreply.github.com>
2023-04-05 21:30:21 -0600

    all changes to mission 14

--- src/MovieList.tsx
+++ src/MovieList.tsx
@@ -1,23 +1,40 @@
-import { useState } from 'react';
-import data from './MovieDataFinal.json';
+import { useEffect, useState } from 'react';
+// import data from './MovieDataFinal.json';
+import { MovieItem } from './types/movieApi';
 
-const movies = data.MovieData;
+// const movies = data.MovieData;
 
 function MovieList() {
-  const [listMovies, setList] = useState(movies);
-  const addMovie = () => {
-    setList([
-      ...movies,
-      {
-        Category: 'Action/Adventure',
-        Title: 'Batman',
-        Year: 1989,
-        Director: 'Tim Burton',
-        Rating: 'PG-13',
-        Edited: 'No',
-      },
-    ]);
-  };
+  const [listMovies, setList] = useState<MovieItem[]>([]);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const rsp = await fetch('https://localhost:4000/movie');
+      const temp = await rsp.json();
+      const filteredList = temp.filter(
+        (movie: MovieItem) => movie.edited.toLowerCase() === 'yes'
+      );
+      const sortedList = filteredList.sort((a: MovieItem, b: MovieItem) =>
+        a.title.localeCompare(b.title)
+      );
+      setList(sortedList);
+    };
+    fetchMovie();
+  }, []);
+
+  // const addMovie = () => {
+  //   setList([
+  //     ...movies,
+  //     {
+  //       Category: 'Action/Adventure',
+  //       Title: 'Batman',
+  //       Year: 1989,
+  //       Director: 'Tim Burton',
+  //       Rating: 'PG-13',
+  //       Edited: 'No',
+  //     },
+  //   ]);
+  // };
   return (
     <>
       {/* <button className="btn btn-primary" onClick={addMovie}>
@@ -31,23 +48,29 @@ function MovieList() {
           <table className="table border ">
             <thead>
               <tr>
+                {/* <th>MovieId</th> */}
                 <th>Title</th>
                 <th>Year</th>
                 <th>Director</th>
                 <th>Rating</th>
                 <th>Category</th>
                 <th>Edited</th>
+                <th>Lent To</th>
+                <th>Notes</th>
               </tr>
             </thead>
             <tbody>
               {listMovies.map((m) => (
-                <tr>
-                  <td>{m.Title}</td>
-                  <td>{m.Year}</td>
-                  <td>{m.Director}</td>
-                  <td>{m.Rating}</td>
-                  <td>{m.Category}</td>
-                  <td>{m.Edited}</td>
+                <tr key={m.movieId}>
+                  {/* <td>{m.movieId}</td> */}
+                  <td>{m.title}</td>
+                  <td>{m.year}</td>
+                  <td>{m.director}</td>
+                  <td>{m.rating}</td>
+                  <td>{m.category}</td>
+                  <td>{m.edited}</td>
+                  <td>{m.lentTo}</td>
+                  <td>{m.notes}</td>
                 </tr>
               ))}
             </tbody>
---
+++ src/types/movieApi.ts
@@ -0,0 +1,11 @@
+export type MovieItem = {
+  movieId: number;
+  category: string;
+  title: string;
+  year: number;
+  director: string;
+  rating: string;
+  edited: string;
+  lentTo: string;
+  notes: string;
+};
